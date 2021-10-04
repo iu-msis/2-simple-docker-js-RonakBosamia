0a50ee892b13c4829410b3df49009ab016c8ac74
RonakBosamia <89400350+RonakBosamia@users.noreply.github.com>
2021-10-04 12:09:45 -0400

    fixed problem

--- app/public/api/student/index.php
+++ app/public/api/student/index.php
@@ -20,4 +20,9 @@ $stmt->execute($vars);
 
 $patients = $stmt->fetchAll();
 
-// Step 3: Convert to  
+// Step 3: Convert to JSON
+$json = json_encode($patients, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
