fcb712dc1dda03ef6b63e71248ddf2e74f0b92fa
RonakBosamia <89400350+RonakBosamia@users.noreply.github.com>
2021-10-04 12:06:27 -0400

    got php to show as json

--- app/public/api/student/index.php
+++ app/public/api/student/index.php
@@ -20,8 +20,4 @@ $stmt->execute($vars);
 
 $patients = $stmt->fetchAll();
 
-// Step 3: Convert to JSON
-$json = json_encode($patients, JSON_PRETTY_PRINT);
-
-// Step 4: Output
-header('Content-Type: application/json');
+// Step 3: Convert to  
