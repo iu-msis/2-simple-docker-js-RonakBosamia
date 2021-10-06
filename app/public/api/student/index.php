dc80e63a40c16fd7ebcab049a5392f686659ccfe
RonakBosamia <89400350+RonakBosamia@users.noreply.github.com>
2021-10-06 16:02:55 -0400

    got json to print out for table information stored in aws cloud

--- app/public/api/student/index.php
+++ app/public/api/student/index.php
@@ -6,14 +6,14 @@ require 'class/DbConnection.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM student';
+$sql = 'SELECT * FROM books';
 $vars = [];
 
-if (isset($_GET['student'])) {
+//if (isset($_GET['books'])) {
 // This is an example of a parameterized query
-$sql = 'SELECT * FROM offer WHERE studentID = ?'; //offer and studentID should be changed for book table if studnet var passed - only that one else all of them
-$vars = [ $_GET['student'] ]; //in browser realting to ^ whatever you type in the url ...8080/api/offer/?student=2
-}
+//$sql = 'SELECT * FROM __ WHERE id = ?'; //offer and studentID should be changed for book table if studnet var passed - only that one else all of them
+//$vars = [ $_GET['student'] ]; //in browser realting to ^ whatever you type in the url ...8080/api/offer/?student=2
+//}
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
